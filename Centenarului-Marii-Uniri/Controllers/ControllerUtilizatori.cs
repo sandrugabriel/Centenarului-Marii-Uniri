be8c45a1947e197134217fbb31de137ded5c48bc
Sandru Gabriel <gabi200611@yahoo.com>
2023-06-14 20:12:57 +0300

    done

---
+++ Centenarului-Marii-Uniri/Controllers/ControllerUtilizatori.cs
@@ -0,0 +1,47 @@
+﻿using Centenarului_Marii_Uniri.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Centenarului_Marii_Uniri.Controllers
+{
+    internal class ControllerUtilizatori
+    {
+
+        private List<Utilizator> utilizatori;
+
+        public ControllerUtilizatori()
+        {
+
+            utilizatori = new List<Utilizator>();
+            load();
+
+        }
+
+        public void load()
+        {
+
+            string path = Application.StartupPath + @"/data/utilizatori.txt";
+
+            StreamReader streamReader = new StreamReader(path);
+
+            string text;
+
+            while((text = streamReader.ReadLine()) != null)
+            {
+
+                Utilizator utilizator = new Utilizator(text);
+                utilizatori.Add(utilizator);
+
+            }
+
+            streamReader.Close();
+        }
+
+
+    }
+}
